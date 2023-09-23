832655b629eeb304bf30f6215d7a228977620087
didimbote-joao <didimbote_joao@outlook.com>
2023-09-23 21:04:43 +0100

    adjust the php comands

--- php/validation.php
+++ php/validation.php
@@ -9,10 +9,10 @@
     $senha = ($_POST['senha']);
     
     //Executa a query
-    $selection = "SELECT TOP 1 `id`, `nome`, `nivel` FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+    $selection = "SELECT `id`, `nome`, `nivel` FROM usuarios WHERE email = '$email' AND senha = '$senha' LIMIT 1";
     $result = $conexao->query($selection);
 
-    if (mysqli_num_rows<1) {
+    if (mysqli_num_rows($result)<1) {
       //Se retornar 0, entao o usuario e/ou senha nao existe
       unset($_POST['email']);
       unset($_POST['senha']);
@@ -26,16 +26,16 @@
       if (!isset($_SESSION)) session_start();
 
       // Salva os dados encontrados na sessão
-      $_SESSION['ID'] = $result['id'];
-      $_SESSION['nome'] = $result['nome'];
-      $_SESSION['nivel'] = $result['nivel'];
+      $_SESSION['id'] = $keepdata['id'];
+      $_SESSION['nome'] = $keepdata['nome'];
+      $_SESSION['nivel'] = $keepdata['nivel'];
 
       #Direcionando a pagina segundo o nivel de acesso
       if ($_SESSION['nivel'] == 2) {
-        header('Location: admin.html'); exit;
+        header('Location: ../pages/admin.html'); exit;
       }
       else if ($_SESSION['nivel'] == 1) {
-        header('Location: usuario.html'); exit;
+        header('Location: ../pages/usuario.html'); exit;
       }
     }
   }
