25de13bb554bec4e118f6f12b1137a1d51cd5334
didimbote-joao <didimbote_joao@outlook.com>
2023-09-26 11:16:22 +0100

    add security and level acess

--- pages/admin.php
+++ pages/admin.php
@@ -1,11 +1,37 @@
+<?php
+  #Inicia a sessao
+  if (!isset($_SESSION)) {
+    session_start();
+  }
+  
+  #Define o nivel de acesso
+  $nivel_requerido = 2; 
+  // print_r($_SESSION['nivel']);
+  
+  #Verifica a existencia de um ID
+  if (!isset($_SESSION['id'])) {
+    // Destrói a sessão por segurança
+    session_destroy();
+    // Redireciona o visitante de volta pra login
+    header("Location: ../index.html"); exit;
+  }
+  #Caso exista ID, analisa o nivel de acesso
+  else if($_SESSION['nivel'] != $nivel_requerido){
+    // Destrói a sessão por segurança
+    session_destroy();
+    // Redireciona o visitante de volta pra login
+    header("Location: ../index.html"); exit;
+  }
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Administrador</title>
 </head>
 <body>
-  
+   <a href="#">Sair</a>
 </body>
 </html>
--- pages/usuario.php
+++ pages/usuario.php
@@ -1,11 +1,36 @@
+<?php
+  #Inicia a sessao
+  if (!isset($_SESSION)) {
+    session_start();
+  }
+  
+  #Define o nivel de acesso
+  $nivel_requerido = 1; 
+  
+  #Verifica a existencia de um ID
+  if (!isset($_SESSION['id'])) {
+    // Destrói a sessão por segurança
+    session_destroy();
+    // Redireciona o visitante de volta pra login
+    header("Location: ../index.html"); exit;
+  }
+  #Caso exista ID, analisa o nivel de acesso
+  else if($_SESSION['nivel'] != $nivel_requerido){
+    // Destrói a sessão por segurança
+    session_destroy();
+    // Redireciona o visitante de volta pra login
+    header("Location: ../index.html"); exit;
+  }
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Usuario</title>
   </head>
   <body>
-    
+    <a href="../php/logout.php">Sair</a>
   </body>
 </html>
